9946394ac76a599647d37d45aaf3cd3beaeea7b7
Vikicha089 <vbanchevf@gmail.com>
2026-06-07 22:19:12 +0300

    Add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router'
-import './App.css'
-import Home from './pages/Home'
-import NavBar from './components/NavBar'
+// import './App.css'
+import Home from './pages/Home/Home'
+import NavBar from './components/NavBar/NavBar'
+import LoginPage from './pages/Auth/Login'
+import RegisterPage from './pages/Auth/Register'
 
 function App() {
 	return (
@@ -10,6 +12,8 @@ function App() {
 			
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
 			</Routes>
 		</>
 	)
