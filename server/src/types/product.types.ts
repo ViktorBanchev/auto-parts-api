f0a07b5bc7e1a1a7915de636104e0b81540b5f25
Vikicha089 <vbanchevf@gmail.com>
2026-06-07 23:04:13 +0300

    Create product data type

---
+++ server/src/types/product.types.ts
@@ -0,0 +1,9 @@
+export interface ProductData {
+    title: string;
+    slug: string;
+    description: string;
+    price: number;
+    image_url: string;
+    category_id: number;
+    brand_id: number;
+}
