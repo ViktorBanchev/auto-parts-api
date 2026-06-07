55882af476e8fa903f7d26521b8dd48fc02b77fe
Vikicha089 <vbanchevf@gmail.com>
2026-06-07 23:05:58 +0300

    Add routes for post and delete

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -1,5 +1,5 @@
 import { Router, type Request, type Response } from "express";
-import { getAllProducts } from "../services/productService.js";
+import { createProduct, deleteProduct, getAllProducts } from "../services/productService.js";
 
 const productController = Router();
 
@@ -10,6 +10,32 @@ productController.get('/', async (req: Request, res: Response) => {
     }
     catch (err) {
         res.status(500).json({ message: "Server error" })
+    }
+});
+
+productController.post('/', async (req: Request, res: Response) => {
+    try {
+        const productData = req.body;
+        const newProduct = await createProduct(productData);
+        res.status(201).json(newProduct)
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+productController.delete('/:productId', async (req: Request, res: Response) => {
+    try {
+        const productId = req.params.productId as string;
+        const isDeleted = await deleteProduct(productId);
+
+        if (!isDeleted) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+
+        res.status(200).json({ message: "Product deleted" });
+    } catch (error) {
+        console.error("Error deleting product:", error);
+        res.status(500).json({ message: "Server error during delete" });
     }
 })
 
