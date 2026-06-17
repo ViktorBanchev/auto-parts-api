9f53b14f619137994a62138caacb700601853f9a
Vikicha089 <vbanchevf@gmail.com>
2026-06-17 17:32:27 +0300

    Add /auth to routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import productController from "./controllers/productController.js";
+import userController from "./controllers/userController.js";
 
 const routes = Router();
 
 routes.use('/products', productController);
+routes.use('/auth', userController);
 
 export default routes;
