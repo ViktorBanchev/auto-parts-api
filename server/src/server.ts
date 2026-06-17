13e9fc409e84d8e921a293e3215eb5e3e1d6a71c
Vikicha089 <vbanchevf@gmail.com>
2026-06-17 17:34:58 +0300

    Update server.ts
    - update cors
    - use cookieParser
    - return number instead of strings

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,23 @@ import express, { type Request, type Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import routes from './routes.js';
+import cookieParser from 'cookie-parser';
+import { types } from 'pg';
 
 dotenv.config();
 
+types.setTypeParser(1700, function(val) {
+    return parseFloat(val);
+})
+
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: "Typescript API for car parts works"})
