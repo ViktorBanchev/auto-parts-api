fa559c66068f9feba1e87cd2b2b3bfcbd012cd99
Vikicha089 <vbanchevf@gmail.com>
2026-06-17 17:28:40 +0300

    Create User model with getByEmail and create

---
+++ server/src/models/User.ts
@@ -0,0 +1,33 @@
+import pool from '../db.js';
+import type { UserData } from '../types/user.types.js';
+
+class UserModel {
+    static async getByEmail(email: string) {
+        const query = `SELECT
+            id, email, password_hash,
+            first_name AS "firstName",
+            last_name AS "lastName",
+            created_at AS "createdAt"
+        FROM users WHERE email = $1`;
+        const result = await pool.query(query, [email]);
+        console.log(result);
+        console.log(result.rows);
+        return result.rows[0];
+    };
+
+    static async createUser(userData: UserData) {
+        const { email, passwordHash, firstName, lastName } = userData;
+        const query = `
+            INSERT INTO users (email, password_hash, first_name, last_name)
+            VALUES ($1, $2, $3, $4)
+            RETURNING id, email,
+                first_name AS "firstName",
+                last_name AS "lastName",
+                created_at AS "createdAt";
+        `;
+        const result = await pool.query(query, [email, passwordHash, firstName, lastName]);
+        return result.rows[0]
+    }
+}
+
+export default UserModel;
