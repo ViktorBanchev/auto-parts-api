780473de8ff202b59a0e9d73ca16ae396ec2c290
Vikicha089 <vbanchevf@gmail.com>
2026-06-17 17:55:22 +0300

    Add new routes and use route guard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router'
-// import './App.css'
 import Home from './pages/Home/Home'
 import NavBar from './components/NavBar/NavBar'
 import LoginPage from './pages/Auth/Login'
 import RegisterPage from './pages/Auth/Register'
+import Cart from './pages/Cart/Cart'
+import ProtectedRoute from './components/ProtectedRoute'
+import ProductsPage from './pages/Products/Products'
+import ProductDetails from './pages/ProductDetails/ProductDetails'
 
 function App() {
 	return (
@@ -14,6 +17,13 @@ function App() {
 				<Route path='/' element={<Home />} />
 				<Route path='/login' element={<LoginPage />} />
 				<Route path='/register' element={<RegisterPage />} />
+				<Route path='/cart' element={<Cart />} />
+				<Route path='/products' element={<ProductsPage />} />
+				<Route path='/products/:slug' element={<ProductDetails />} />
+
+				<Route element={<ProtectedRoute />}>
+					<Route path='/checkout' element={(<h2>Checkout page</h2>)} />
+				</Route>
 			</Routes>
 		</>
 	)
