42abcd384f0fdab9e707f93f1b7cfac45c2dfa8a
Vikicha089 <vbanchevf@gmail.com>
2026-06-17 17:49:23 +0300

    Update navbar
    - add logout logic
    - add conditional rendering

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,33 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import styles from './NavBar.module.css'
+import { useAuthStore } from "../../store/authStore";
+import { useMutation } from "@tanstack/react-query";
+import { logoutUser } from "../../services/authService";
+import { useCartStore } from "../../store/cartStore";
 
 export default function NavBar() {
+    const navigate = useNavigate();
+    const { isAuthenticated, user, logout} = useAuthStore();
+
+    const cartItems = useCartStore((state) => state.items);
+
+    const totalItemsCount = cartItems.reduce((sum, item) => sum + item.quantity, 0);
+
+    const logoutMutation = useMutation({
+        mutationFn: logoutUser,
+        onSuccess: () => {
+            logout();
+            navigate('/');
+        },
+        onError: (error) => {
+            console.error('Error during logout', error)
+        }
+    });
+
+    const handleLogout = () => {
+        logoutMutation.mutate();
+    }
+
     return (
         <nav className={styles.navbar}>
             <div className={styles.logoContainer}>
@@ -11,8 +37,26 @@ export default function NavBar() {
             <ul className={styles.navLinks}>
                 <li><Link to="/" className={styles.link}>Home</Link></li>
                 <li><Link to="/products" className={styles.link}>Products</Link></li>
-                <li><Link to="/cart" className={styles.cartButton}>Cart (0)</Link></li>
-                <li><Link to="/login" className={styles.link}>Sign in</Link></li>
+                <li><Link to="/cart" className={styles.cartButton}>Cart ({totalItemsCount})</Link></li>
+                {isAuthenticated ?
+                    (
+                        <>
+                            <li className={styles.greeting}>Hello, {user?.firstName}</li>
+                            <li>
+                                <button onClick={handleLogout} className={styles.link} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
+                                    Logout
+                                </button>
+                            </li>
+                        </>
+                    ) :
+                    (
+                        <>
+                            <li><Link to="/login" className={styles.link}>Sign in</Link></li>
+                            <li><Link to="/register" className={styles.link}>Sign up</Link></li>
+                        </>
+                    )
+                }
+
             </ul>
         </nav>
     )
