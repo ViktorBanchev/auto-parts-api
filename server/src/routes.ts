3eca42e4fa6b48b42dd7ab1bc045d9c29ae6bd4f
Vikicha089 <vbanchevf@gmail.com>
2026-06-26 20:08:13 +0300

    feat(routes): add /categories endpoint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import productController from "./controllers/productController.js";
 import userController from "./controllers/userController.js";
+import categoryController from "./controllers/categoryController.js";
 
 const routes = Router();
 
 routes.use('/products', productController);
 routes.use('/auth', userController);
+routes.use('/categories', categoryController);
 
 export default routes;
