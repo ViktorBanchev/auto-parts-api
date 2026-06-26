b3883decc93720dcc98ee3218004ebefbd1f29ec
Vikicha089 <vbanchevf@gmail.com>
2026-06-26 21:01:57 +0300

    feat(client): implement filters in products catalog

--- client/src/pages/Products/Products.tsx
+++ client/src/pages/Products/Products.tsx
@@ -2,25 +2,79 @@ import { useQuery } from '@tanstack/react-query';
 import { getProducts } from '../../services/productService';
 import ProductCard from '../../components/ProductCard/ProductCard';
 import styles from './Products.module.css';
+import { useState } from 'react';
+import { useSearchParams } from 'react-router';
+import FilterDrawer from './FilterDrawer';
 
 export default function ProductsPage() {
+    const [isFilterOpen, setIsFilterOpen] = useState(false);
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const categoryParam = searchParams.get('category');
+    const searchQuery = searchParams.get('search');
+    const page = Number(searchParams.get('page')) || 1;
+    const limit = Number(searchParams.get('limit')) || 5;
+
+    const selectedCategories = categoryParam ? categoryParam.split(',') : [];
+
     const { data: products, isLoading, error } = useQuery({
-        queryKey: ['products'],
-        queryFn: getProducts
+        queryKey: ['products', categoryParam, searchQuery],
+        queryFn: () => getProducts(categoryParam, searchQuery, page, limit),
+        placeholderData: (previousData) => previousData
     });
+
+    const handleCategoryChange = (categorySlug: string, isChecked: boolean) => {
+        let updatedCategories = [...selectedCategories];
+        
+        if (isChecked) {
+            updatedCategories.push(categorySlug)
+        } else {
+            updatedCategories = updatedCategories.filter(c => c !== categorySlug)
+        }
+
+        if (updatedCategories.length > 0) {
+            searchParams.set('category', updatedCategories.join(','));
+        } else {
+            searchParams.delete('category');
+        }
+        setSearchParams(searchParams);
+    }
+
+    const clearFilters = () => {
+        setSearchParams({});
+    };
 
     if (isLoading) return <div>Loading catalog...</div>;
     if (error) return <div>Error loading catalog.</div>;
 
+
+
     return (
         <main className={styles.catalogContainer}>
             <h1 className={styles.catalogTitle}>All Auto Parts</h1>
+            <div>
+                <button
+                    className={styles.filterBtn}
+                    onClick={() => setIsFilterOpen(true)}
+                >
+                    Filters
+                </button>
+            </div>
 
             <div className={styles.productGrid}>
                 {products?.map((product: any) => (
                     <ProductCard key={product.slug} {...product} />
                 ))}
             </div>
+
+            <FilterDrawer
+                isOpen={isFilterOpen}
+                onClose={() => setIsFilterOpen(false)}
+                selectedCategories={selectedCategories}
+                onCategoryChange={handleCategoryChange}
+                onClearFilters={clearFilters}
+            />
+
         </main>
     );
 }
