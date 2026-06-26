b827310267fb452193a1fcfb039a8410e25d88b3
Vikicha089 <vbanchevf@gmail.com>
2026-06-26 21:37:42 +0300

    improve db setup

--- server/src/db.ts
+++ server/src/db.ts
@@ -3,14 +3,20 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    connectionString: process.env.DB_URL,
-    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
-});
+const pool = new Pool(
+    process.env.DATABASE_URL 
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false } // Почти винаги нужно за облачни бази
+      }
+    : {
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        ssl: false // Локално обикновено нямаме SSL
+      }
+);
 
 export default pool;
