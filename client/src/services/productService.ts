9da92f99a147c5c470b84f18b60ffe2e20ff3e3f
Vikicha089 <vbanchevf@gmail.com>
2026-06-26 20:56:20 +0300

    feat(client): upgrade get products method with filters

--- client/src/services/productService.ts
+++ client/src/services/productService.ts
@@ -11,12 +11,33 @@ export interface Product {
     brand_id: string;
 }
 
-export const getProducts = async (): Promise<Product[]> => {
-    const response = await api.get('/products');
-    return response.data;
+interface Filters {
+    categoryParam: string | undefined;
+    searchQuery: string | undefined;
+    page: number;
+    limit: number;
+}
+
+export const getProducts = async (
+    categoryParam: string | null,
+    searchQuery: string | null,
+    page: number = 1,
+    limit: number = 12
+): Promise<Product[]> => {
+    let url = '/products'
+    const response = await api.get(url, {
+        params: {
+            category: categoryParam || undefined,
+            search: searchQuery || undefined,
+            page: page,
+            limit: limit
+        }
+    });
+
+    return response.data.data;
 }
 
 export const getProductBySlug = async (slug: string): Promise<Product> => {
     const response = await api.get(`/products/${slug}`);
-    return response.data;
+    return response.data.data;
 }
