183f2b2dcc0c888804553cb2c1268ef767cc8544
Vikicha089 <vbanchevf@gmail.com>
2026-06-27 11:04:23 +0300

    fix(cors)

--- server/src/server.ts
+++ server/src/server.ts
@@ -14,8 +14,10 @@ types.setTypeParser(1700, function(val) {
 const app = express();
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
-    credentials: true
+    origin: process.env.CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
 app.use(cookieParser());
